7dcc94b6d10e6a5a314a44b6422187082ed83e9f
TheuzGod <101230469+TheuzGod@users.noreply.github.com>
2022-03-12 23:47:33 -0300

    Delete SRD.py

--- SRD.py
+++
@@ -1,9 +0,0 @@
-### SRD significa Simulador de Rolagem de Dados
-
-from random import randint
-repeat_rolling = True
-while repeat_rolling:
-    print("Você rolou o seguinte número usando os dados :",randint(-200,200))
-    print("Deseja jogar os dados novamente?")
-    repeat_rolling = ("s" or "sim") in input().lower()
-    
